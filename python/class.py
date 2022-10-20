e5821988f89b15a59d52fd3232c0d4909679574e
Muhammad Rizkillah <65638961+rizkidotme@users.noreply.github.com>
2022-10-20 22:16:35 +0000

    "cc selesai"

---
+++ python/class.py
@@ -0,0 +1,11 @@
+class Dog: 
+    def __init__(self, name, age): 
+        self.name = name
+        self.age = age
+    
+    def sit(self): 
+        print(f"{self.name} is not sitting")
+    
+    def roll_over(self):
+        print(f"{self.name} is now rolled over")
+
